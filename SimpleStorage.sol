58dac60619db57a7b045b6335f7923981151617e
akshit-dev101 <akshitkumar540@gmail.com>
2025-02-25 08:04:10 +0530

    initial commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract SimpleStorage{
+    uint256 myFavoriteNumber;
+    // uint256[] listOfFavoriteNumbers;
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+
+    }
+    // Person public pat=Person({favoriteNumber:7,name:"Pat"});
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    Person[] public listOfPeople; //[]
+    function store(uint _favoriteNumber) public{
+            myFavoriteNumber=_favoriteNumber;
+
+    }
+    function retrive() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+function addPerson(string memory _name, uint256 _favoriteNumber) public {
+    listOfPeople.push(Person(_favoriteNumber,_name));
+    nameToFavoriteNumber[_name]=_favoriteNumber;
+}
+
+}
